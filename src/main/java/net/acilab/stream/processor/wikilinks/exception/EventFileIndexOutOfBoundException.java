1969496451fa8a0058421a8d4371a888b64309c7
Eric Guo <eric.koyn@gmail.com>
2021-01-30 23:06:23 +1100

    Fix warning: EventFileIndexOutOfBoundException the serializable class does not declare a static final ...

--- src/main/java/net/acilab/stream/processor/wikilinks/exception/EventFileIndexOutOfBoundException.java
+++ src/main/java/net/acilab/stream/processor/wikilinks/exception/EventFileIndexOutOfBoundException.java
@@ -1,6 +1,7 @@
 package net.acilab.stream.processor.wikilinks.exception;
 
 public class EventFileIndexOutOfBoundException extends Exception {
+  private static final long serialVersionUID = 1L;
 
   public EventFileIndexOutOfBoundException(String message, Throwable cause) {
     super(message, cause);
